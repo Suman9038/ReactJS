6e214957afb1325efb4f06ec7557a53abf79ec16
Suman9038 <sumanjhad903878@gmail.com>
2026-01-20 15:13:24 +0530

    First Commit

---
+++ Basics Things/app.js
@@ -0,0 +1,12 @@
+var a = 10
+
+export let user = "Jhoe"
+
+export default a 
+
+
+
+// Export  do tarike k hote hai
+// 1 named export and 2 default export
+// Named export means kisi ko naam deke export karna hai
+// Default export means har file kuch na kuch send karti hai 
---
+++ Day-1/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+      <div class="Papa">
+        <h1>Hello I am from APP.jsx</h1>
+      </div>
+      <div className="chacha">
+        <h1>I am chaha</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
